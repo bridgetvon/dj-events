9618bfbc01bf102349dd27f4c1fd750ee30cfa65
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-03-21 14:50:35 -0600

    added header and footer and basic styles

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import Layout from "../../components/Layout"
 
 export default function EventsPage() {
   return (
-    <div>
+    <Layout>
         <h1>My events</h1>
-    </div>
+    </Layout>
   )
 }
